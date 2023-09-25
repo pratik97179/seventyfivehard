b087899742329070f6c8ac36316a50b4fd1fdea5
Pratik Nath Tiwari <pratik@techalchemy.co>
2023-09-26 04:23:14 +0530

    Routing function included

---
+++ lib/utility/routing/routing_functions.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+/// Class that contains string constants for all routes used in the app
+class RoutingFunctions {
+  /// Handles routes that can't be handled using simple named routes map.
+  static Route<dynamic>? generateRoutes(RouteSettings settings) {
+    print("generateRoutes($settings)");
+    Uri routeUri = Uri.parse(settings.name!);
+
+    Widget? target;
+
+    switch (routeUri.path) {
+      
+    }
+
+    if (target != null) {
+      print("Found route, opening it");
+      return createRoute(target);
+    } else {
+      print("Unknown route found");
+      return null;
+    }
+  }
+
+  /// Function used to create custom animated route
+  static Route createRoute(Widget widget) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => widget,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0, 0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+}
